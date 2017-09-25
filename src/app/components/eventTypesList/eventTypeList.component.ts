8ffa5ed11e791979a0db09f7fde969dff591f237
cakeinpanic <cakeinpanic@gmail.com>
2017-09-26 00:02:42 +0300

    Improve event type removing

--- src/app/components/eventTypesList/eventTypeList.component.ts
+++ src/app/components/eventTypesList/eventTypeList.component.ts
@@ -2,60 +2,68 @@ import {Component, OnInit, Inject, Output, EventEmitter} from '@angular/core';
 import {EventTypeStore, EventType} from '../models/eventType.store';
 import {AlertController} from 'ionic-angular';
 import {CalendarStore} from '../models/calendar.store';
+import {DayEvent} from '../models/dayEvent.model';
+import * as _ from 'lodash';
+import * as moment from 'moment';
 
 @Component({
     selector: 'event-type-list',
     templateUrl: './eventTypeList.template.html'
 })
 export class EventTypeList implements OnInit {
-    
+
     @Output() onTypeSelect = new EventEmitter<EventType>();
     @Output() onAddNewClick = new EventEmitter<EventType>();
     types: EventType[];
-    
+
     constructor(@Inject(EventTypeStore) private eventTypeStore: EventTypeStore,
                 @Inject(AlertController) private alertCtrl: AlertController,
                 @Inject(CalendarStore) private calendarStore: CalendarStore) {
-        
+
     }
-    
+
     ngOnInit() {
         this.updateTypes();
         this.eventTypeStore.updateStream.subscribe(() => {
             this.updateTypes();
         });
     }
-    
+
     edit(type: EventType) {
         this.onTypeSelect.emit(type);
     }
-    
+
     updateTypes() {
         this.eventTypeStore.getAllTypes().then(types => {
             this.types = types;
         });
     }
-    
+
     removeType(event: MouseEvent, type: EventType) {
         event.stopPropagation();
-        this.calendarStore.hasEventsOfType(type.id)
-            .then((hasEvents: boolean) => {
-                if (hasEvents) {
+        this.calendarStore.getEventsOfType(type.id)
+            .then((events: moment.Moment[]) => {
+                if (events.length) {
+                    console.log(events);
+                    let daysList = _.take(events, 2).map(event => event.format('DD/MM/YY')).join(',');
+                    if (daysList.length > 2) {
+                        daysList += '...';
+                    }
                     let alert = this.alertCtrl.create({
                         title: 'Can\'t remove type',
-                        subTitle: 'There are event of this type',
+                        subTitle: `There are event(s) of this type on days:${daysList}`,
                         buttons: ['Ok']
                     });
-                    
+
                     alert.present();
                     return;
                 }
                 this.eventTypeStore.removeType(type.id);
             });
-        
+
     }
-    
+
     addNewEventType() {
-        this.onAddNewClick.emit();
+        this.onTypeSelect.emit(new EventType());
     }
 }
--- src/app/components/models/calendar.store.ts
+++ src/app/components/models/calendar.store.ts
@@ -9,16 +9,16 @@ import {Storage} from '@ionic/Storage';
 
 @Injectable()
 export class CalendarStore {
-    
+
     private store: { [key: string]: DayEvent[] } = {};
     private stream = new Subject<number>();
-    
+
     private affectedIds: number[] = [];
-    
+
     constructor(@Inject(EventTypeStore) private eventTypeStore: EventTypeStore,
                 @Inject(Storage) private storage: Storage) {
     }
-    
+
     init(): Promise<any> {
         return this.getAllFromStore()
             .then(() => {
@@ -26,7 +26,7 @@ export class CalendarStore {
                 this.affectedIds.length = 0;
             });
     }
-    
+
     syncData(): Promise<any> {
         return Promise.all(
             this.affectedIds.map((id) => {
@@ -40,17 +40,22 @@ export class CalendarStore {
                 this.affectedIds.length = 0;
             });
     }
-    
-    hasEventsOfType(typeId: number): Promise<boolean> {
+
+    getEventsOfType(typeId: number): Promise<moment.Moment[]> {
         return Promise.resolve(
-            _.some(this.store, (events) => events.filter((event: DayEvent) => event.isOfTypeId(typeId)))
+            _.map(
+                this.store,
+                (events, key) => events.some((event: DayEvent) => event.isOfTypeId(typeId)) && key
+            )
+            .filter(data => !!data)
+            .map(dayID => {return moment(`/Date(${dayID})/`)})
         );
     }
-    
+
     get eventStream(): Observable<number> {
         return this.stream.asObservable();
     }
-    
+
     getEventsById(dateId: number): Promise<DayEvent[]> {
         return Promise.resolve(
             this.store[dateId]
@@ -58,35 +63,35 @@ export class CalendarStore {
                 : []
         );
     }
-    
+
     removeEvent(date: moment.Moment, eventToDelete: DayEvent) {
         const dateId = this.getDateId(date);
         this.affectedIds.push(dateId);
         _.remove(this.store[dateId], {id: eventToDelete.id});
-        
+
         this.sendNext(dateId);
         this.syncData();
     }
-    
+
     addEvent(date: any, event: DayEvent) {
         const dateId = this.getDateId(date);
         this.affectedIds.push(dateId);
-        
+
         if (this.store[dateId]) {
             this.store[dateId].push(event);
         } else {
             this.store[dateId] = [event];
         }
-        
+
         this.syncData();
         this.sendNext(dateId);
-        
+
     }
-    
+
     getDateId(date: moment.Moment): number {
         return moment(date).hours(0).minutes(0).seconds(0).milliseconds(0).valueOf();
     }
-    
+
     clearAll() {
         this.getEventKeys()
             .then(keys =>
@@ -95,17 +100,17 @@ export class CalendarStore {
                 })
             );
     }
-    
+
     private getEventKeys(): Promise<string[]> {
         return this.storage.keys()
             .then(keys => keys.filter((key) => key.indexOf('event') > -1));
     }
-    
+
     private getAllFromStore(): Promise<any> {
         return this.getEventKeys()
             .then(keys => Promise.all(keys.map((key) => this.getDataFromStorageByKey(key))));
     }
-    
+
     private getDataFromStorageByKey(key: string): Promise<DayEvent[]> {
         let dayId = this.getDateFormKey(key);
         return this.storage.get(key)
@@ -116,7 +121,7 @@ export class CalendarStore {
                 return this.store[dayId] = events;
             });
     }
-    
+
     private dayEventFromJSON(json: DayEventData): Promise<DayEvent> {
         return this.eventTypeStore.getTypeByID(json.typeId)
             .then((type: EventType) => {
@@ -125,17 +130,17 @@ export class CalendarStore {
                 return null;
             });
     }
-    
+
     private sendNext(dateId) {
         this.stream.next(dateId);
     }
-    
+
     private getDateFormKey(key: string): string {
         return /event(\d+)/.exec(key)[1];
     }
-    
+
     private getKeyFromId(id: number): string {
         return `event${id}`;
     }
-    
+
 }
